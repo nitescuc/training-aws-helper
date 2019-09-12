e6fe84dc90876945a040bed67085b0487c678e35
NITESCU Cristian <cristian.nitescu@renault.com>
2019-09-12 10:26:56 +0200

    Added job status event

--- src/s3Helper.js
+++ src/s3Helper.js
@@ -2,6 +2,7 @@ const archiver = require('archiver');
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-1'});
 const s3 = new AWS.S3();
+const sagemaker = new AWS.SageMaker();
 const { PassThrough } = require('stream');
 const EventEmitter = require('events');
 
@@ -32,6 +33,55 @@ class S3Helper extends EventEmitter {
         
         uploadManager.on('httpUploadProgress', progress => this.emit('progress', progress));
         return uploadManager.promise();
+    }
+
+    async createTrainingJob(s3Prefix) {
+        const TrainingJobName = new Date().toISOString().replace(/:|\./g, '-');
+        const job = await sagemaker.createTrainingJob({
+            AlgorithmSpecification: {
+                TrainingInputMode: 'File',
+                TrainingImage: '263430657496.dkr.ecr.eu-west-1.amazonaws.com/robocars:1.8.0-gpu-py3'
+            },
+            OutputDataConfig: {
+                S3OutputPath: 's3://robocars/model'
+            },
+            ResourceConfig: {
+                InstanceType: 'ml.p2.xlarge',
+                InstanceCount: 1,
+                VolumeSizeInGB: 1
+            },
+            RoleArn: 'arn:aws:iam::263430657496:role/service-role/AmazonSageMaker-ExecutionRole-20180512T173485',
+            StoppingCondition: {
+                MaxRuntimeInSeconds: 3600
+            },
+            TrainingJobName,
+            HyperParameters: {
+                sagemaker_region: '"eu-west-1"',
+                use_generator: 'false',
+                count_to_generate: '100',
+                slide: '2'
+            },
+            InputDataConfig: [{
+                ChannelName: 'train', 
+                DataSource: { 
+                    S3DataSource: { 
+                        S3DataType: 'S3Prefix', 
+                        S3Uri: s3Prefix, 
+                        S3DataDistributionType: 'FullyReplicated' 
+                    }
+                } 
+            }]
+        }).promise();
+        
+        const interval = setInterval(async () => {
+            const status = await sagemaker.describeTrainingJob({
+                TrainingJobName
+            }).promise();
+            if (status.TrainingJobStatus !== 'InProgress' && status.TrainingJobStatus !== 'Stopping') clearInterval(interval);
+            this.emit('job_status', status);
+        }, 10000);
+
+        return { TrainingJobName, ...job };
     }
 }
 
--- test/folderUploadTest.js
+++ test/folderUploadTest.js
@@ -6,5 +6,13 @@ const s3Helper = new S3Helper({
     Bucket: 'robocars'
 });
 s3Helper.on('progress', progress => console.log(progress));
-//s3Helper.uploadFolderToZip(path.join(__dirname, '..', 'src'), 'test/test.zip');
-s3Helper.uploadFolderToZip('/Users/az02289/d2/data/home', 'test/test.zip');
+s3Helper.on('job_status', status => console.log(status));
+/*s3Helper.uploadFolderToZip('/Users/az02289/d2/data/home', 'test/test.zip').then(() => {
+    return s3Helper.createTrainingJob('s3://robocars/test');
+}).then(result => {
+    console.log('CreateJob result', result);
+}).catch(e => console.error('Error', e));
+*/
+s3Helper.createTrainingJob('s3://robocars/test').then(result => {
+    console.log('CreateJob result', result);
+}).catch(e => console.error('Error', e));
